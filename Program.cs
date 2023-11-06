8a7818d7057b81c21caf5a316fda0461ca01dcfc
FazeZxc <randomuploader.en@gmail.com>
2023-11-06 21:37:56 +0530

    01

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+
+// change the appearance
+
+Console.Title = "Skynet";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WindowHeight = 20;
 Console.WriteLine("Hello, World!");
+Console.WriteLine("hello whats your name");
+
+
+Console.ReadLine();
 Console.ReadKey();
